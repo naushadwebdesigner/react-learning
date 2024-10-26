1401df5eabe0a434773449a2270be7a36172e9ce
naushadwebdesigner <101995493+naushadwebdesigner@users.noreply.github.com>
2024-10-26 19:38:22 +0000

    Counter

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,11 +1,42 @@
 
 import './App.css';
+import {useState} from 'react'
 
 function App() {
+
+
+  let [counter, setCounter] = useState(1);
+
+  const addNumber = () => {
+
+     
+      setCounter(counter++)
+     
+    
+  }
+
+  const removeNumber = () => {
+    if(counter > 0) {
+      setCounter(counter--)
+    }
+  }
+
+
   return (
-    <div>
-      <h1>Hello World</h1>
-    </div>
+    
+
+    <>
+    
+      <h1>
+        Counter : {counter}
+      </h1>
+
+      <button onClick={addNumber}>Add Number</button>
+      <button onClick={removeNumber}>Remove Number</button>
+
+    
+    </>
+
   );
 }
 
